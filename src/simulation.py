34378bac49226c57a23fa046cba96f1349909fa8
esneider <dariosn@gmail.com>
2014-11-02 01:31:51 -0300

    Fix a couple of bugs

--- src/simulation.py
+++ src/simulation.py
@@ -3,12 +3,12 @@ import random
 import math
 import heapq
 
-NUM_NODES = 10
-CONNECTIONS_PER_NODE = 4
+NUM_NODES = 30
+CONNECTIONS_PER_NODE = 6
 TIME_BETWEEN_BLOCKS_SECS = 600
 MAX_BLOCK_SIZE_KB = 1000
 MAX_BANDWIDTH_KBPS = 10000
-MAX_DELAY_SECS = 0.015
+MAX_DELAY_SECS = 0.200
 
 class Node(object):
     id = 0
@@ -40,25 +40,34 @@ class Block(object):
         self.miner = miner
         self.parent = miner.head
         self.height = self.parent.height + 1 if self.parent else 0
+        if self.parent:
+            assert self.timestamp > self.parent.timestamp
 
 class Event(object):
     def __init__(self, timestamp):
         self.timestamp = timestamp
+
     def __cmp__(self, other):
-        return self.timestamp - other.timestamp
+        if self.timestamp == other.timestamp:
+            return 0
+        return 1 if self.timestamp > other.timestamp else -1
+
+    def __str__(self):
+        return '(%s created at %.3f)' % (type(self).__name__, self.timestamp)
 
 class BlockMined(Event):
     def __init__(self, timestamp, miner):
         super(BlockMined, self).__init__(timestamp)
         self.miner = miner
         self.ignored = False
+        self.accepted = True
 
     def apply(self, network):
         events = []
         self.block = Block(self.timestamp, self.miner)
         self.miner.head = self.block
         events.append(createBlockEvent(network, self.timestamp))
-        events += createPropagationEvents(self.miner, self.block)
+        events += createPropagationEvents(self.miner, self.block, self.block.timestamp)
         return events
 
     def log(self):
@@ -81,7 +90,7 @@ class BlockReceived(Event):
             return []
         self.accepted = True
         self.receiver.head = self.block
-        return createPropagationEvents(self.receiver, self.block)
+        return createPropagationEvents(self.receiver, self.block, self.timestamp)
 
     def log(self):
         return '[%s] node %d received block %d and %s it' % (
@@ -110,8 +119,11 @@ def createRandomNetwork(numNodes, numConnections):
         network.nodes.append(Node(randomBandwidth()))
 
     for node in network.nodes:
-        for _ in xrange(numConnections):
-            node.connections.append(Connection(network.randomNode(), randomDelay()))
+        for _ in xrange(numConnections/2):
+            peer = network.randomNode()
+            delay = randomDelay()
+            node.connections.append(Connection(peer, delay))
+            peer.connections.append(Connection(node, delay))
 
     return network
 
@@ -119,33 +131,65 @@ def createBlockEvent(network, currentTime):
     time = currentTime + randomTimeBetweenBlocks(TIME_BETWEEN_BLOCKS_SECS);
     return BlockMined(time, network.randomNode())
 
-def createPropagationEvents(sender, block):
-    if block == None:
-        raise Error('Block is null')
+def createPropagationEvents(sender, block, timestamp):
     events = []
     for connection in sender.connections:
         receiver = connection.peer
         bandwidth = min(receiver.bandwidth, sender.bandwidth)
-        time = block.timestamp + connection.delay + MAX_BLOCK_SIZE_KB / bandwidth
+        time = timestamp + connection.delay + MAX_BLOCK_SIZE_KB / bandwidth
         # TODO: take into consideration node's total bandwith
         events.append(BlockReceived(time, receiver, block))
     return events
 
+def drawNetwork(network):
+    with open('network.dot', 'w') as f:
+        f.write('graph network {')
+        for node in network.nodes:
+            f.write('\tnode%d [label="%d"];' % (node.id, node.id))
+        for node in network.nodes:
+            for connection in node.connections:
+                if node.id < connection.peer.id:
+                    f.write('\tnode%d -- node%d;' % (node.id, connection.peer.id))
+        f.write('}')
+
+class Events(object):
+    def __init__(self):
+        self.events = []
+        self.lastPop = None
+
+    def push(self, event):
+        assert not self.lastPop or event.timestamp >= self.lastPop.timestamp
+        heapq.heappush(self.events, event)
+
+    def pop(self):
+        event = heapq.heappop(self.events)
+        assert not self.lastPop or event.timestamp >= self.lastPop.timestamp
+        self.lastPop = event
+        return event
+
+    def empty(self):
+        return len(self.events) == 0
+
+
 if __name__ == "__main__":
 
-    events = []
+    events = Events()
     network = createRandomNetwork(NUM_NODES, CONNECTIONS_PER_NODE)
+    drawNetwork(network)
 
-    heapq.heappush(events, createBlockEvent(network, 0))
-    while len(events):
-        event = heapq.heappop(events)
+    events.push(createBlockEvent(network, 0))
+    while not events.empty():
+        event = events.pop()
+        # print event.timestamp
         results = event.apply(network)
         if not event.ignored:
             print event.log()
-        if type(event) == BlockMined:
-            raw_input()
+            if not event.accepted:
+                raw_input()
+        # if type(event) == BlockMined:
+        #     raw_input()
         for event in results:
-            heapq.heappush(events, event)
+            events.push(event)
 
     print network
 
